77207d6dd21fabd09bafc40c5bc9510ebfc6d703
clozzii <885204655@qq.com>
2020-12-18 14:37:23 +0800

    完成用户列表功能的开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,131 @@
 <template>
-  <div>
-    <el-button @click="logout">555</el-button>
-  </div>
+  <el-container class="home-container">
+    <!--  -->
+    <el-header>
+      <div>
+        <img src="../assets/images/heima.png" alt="">
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button @click="logout">退出</el-button>
+    </el-header>
+
+    <el-container>
+        <!-- 侧边栏 -->
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <el-menu :default-active="activePath" router :collapse-transition="false" :collapse="isCollapse" :unique-opened="true" background-color="#333744" text-color="#fff" active-text-color="#409EFF">
+          <!-- 一级菜单 -->
+          <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
+            <template slot="title">
+              <i :class="iconsObj[item.id]"></i>
+              <span>{{item.authName}}</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item  @click="saveNavState('/' + sunItem.path)" :index="'/' + sunItem.path" v-for="sunItem in item.children" :key="sunItem.id">
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>{{sunItem.authName}}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
   export default {
+    data() {
+      return {
+        menulist: [],
+        iconsObj: {
+          '125': 'icon-s el-icon-user-solid',
+          '103': 'icon-s el-icon-s-ticket',
+          '101': 'icon-s el-icon-s-data',
+          '102': 'icon-s el-icon-s-opportunity',
+          '145': 'icon-s el-icon-s-help'
+        },
+        isCollapse: false,
+        activePath: ''
+      }
+    },
+    created() {
+      this.getMenuList()
+      this.activePath = window.sessionStorage.getItem('activePath')
+    },
     methods: {
       logout() {
-        window.sessionStorage.clear()
-        this.$router.push('/login')
+        window.sessionStorage.clear();
+        this.$router.push("/login");
+      },
+      async getMenuList() {
+        const { data: res } = await this.$http.get('menus')
+        if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+        this.menulist = res.data
+        // console.log(res)
+      },
+      toggleCollapse() {
+        this.isCollapse = !this.isCollapse
+      },
+      saveNavState(activePath) {
+        window.sessionStorage.setItem("activePath", activePath)
+        this.activePath = activePath
+      }
+    }
+  };
+</script>
+
+<style lang="less" scoped>
+  .home-container {
+    height: 100%;
+  }
+
+  .el-header {
+    background-color: #373d41;
+    display: flex;
+    justify-content: space-between;
+    padding-left: 0;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+
+    >div {
+      display: flex;
+      align-items: center;
+
+      span {
+        margin-left: 15px;
       }
     }
   }
-</script>
 
-<style scoped></style>
+  .el-aside {
+    background-color: #333744;
+    
+    .el-menu {
+      border-right: none;
+    }
+  }
+
+  .el-main {
+    background-color: #eaedf1;
+  }
+
+  .icon-s {
+    margin-right: 10px;
+  }
+
+  .toggle-button {
+    background-color: #4A5064;
+    font-size: 10px;
+    line-height: 24px;
+    color: #fff;
+    text-align: center;
+    letter-spacing: 0.2em;
+    cursor: pointer;
+  }
+</style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
-import { Button, Form, FormTtem, Input, Message } from 'element-ui'
+import { Button, Form, FormTtem, Input, Message, MessageBox } from 'element-ui'
 
 Vue.use(Button, Form, FormTtem, Input, Message)
 
 Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.$confirm
