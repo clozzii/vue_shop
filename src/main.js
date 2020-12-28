b2519ffd62e13211a2870869554da813d5644ce5
clozzii <885204655@qq.com>
2020-12-28 19:36:57 +0800

    完成商品功能开发

--- src/main.js
+++ src/main.js
@@ -9,7 +9,28 @@ import './assets/css/global.css'
 import axios from 'axios'
 
 import TreeTable from 'vue-table-with-tree-grid'
+
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
+
 Vue.component('tree-table', TreeTable)
+Vue.use(VueQuillEditor)
+
+Vue.filter('dateFormat', function(originVal) {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1+ '').padStart(2, 0)
+  const d = (dt.getDate() + '').padStart(2, 0)
+  
+  const hh = (dt.getHours() + '').padStart(2, 0)
+  const mm = (dt.getMinutes() + '').padStart(2, 0)
+  const ss = (dt.getSeconds() + '').padStart(2, 0)
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 Vue.use(ElementUI)
 // Vue.prototype.$message = Message
